71b4278736de9c3fa42d4e6f3a43a31a1b6048d1
Outcastshivam <shivamkr9303695@gmail.com>
2024-12-31 18:51:40 +0530

    problem - 3

---
+++ 1401.CircleandRectangleOverlapping.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        auto getMinimumDistance = [](int minEdge, int maxEdge, int centerCoord) -> int {
+            if (centerCoord >= minEdge && centerCoord <= maxEdge) {
+                return 0;
+            }
+            return centerCoord < minEdge ? minEdge - centerCoord : centerCoord - maxEdge;
+        };
+      
+        int deltaX = getMinimumDistance(x1, x2, xCenter);
+        int deltaY = getMinimumDistance(y1, y2, yCenter);
+      
+        return deltaX * deltaX + deltaY * deltaY <= radius * radius;
+    }
